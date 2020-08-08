9681a85f662b302583e8d7fc6dda699ce9023215
Eudes Guzmán <eudesgg19@gmail.com>
2020-08-08 10:27:27 +0000

    simple counter

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,27 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
+//import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [counter, setCounter] = useState(0);
+	const [counter2, setCounter2] = useState(0);
+
+	useEffect(() => {
+		setInterval(() => {
+			setCounter(counter => counter + 1);
+		}, 1000);
+	}, []);
+
+	if (counter == 10) {
+		setCounter(0);
+		setCounter2(counter2 => counter2 + 1);
+	}
+
+	return (
+		<div className="container">
+			<h1>Counter</h1>
+			<div className="target">{counter2}</div>
+			<div className="target">{counter}</div>
+		</div>
+	);
+};
